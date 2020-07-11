09b90d22684ae6da1d1d4689090357e0b6aa0a5f
Gabriel Ortega <gabrielo_9103@hotmail.com>
2020-07-11 00:08:07 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const PORT = 8081 || process.env.PORT;
+
+app.get("", (req, res) => {
+  const time = new Date();
+  res.send({ hello: `from my service: ${time.toString()}` });
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
